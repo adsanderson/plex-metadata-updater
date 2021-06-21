eb53034696c5c28eab748b72296d1831b84d0d57
Adam Sanderson <adam@adamsanderson.co.uk>
2021-06-21 09:25:39 +0000

    try different plex server, no luck

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,2 +1,4 @@
 export const PLEX_USERNAME = import.meta.env.VITE_PLEX_USERNAME as string;
 export const PLEX_PASSWORD = import.meta.env.VITE_PLEX_PASSWORD as string;
+export const PLEX_SERVER = import.meta.env.VITE_PLEX_SERVER as string;
+export const PLEX_PORT = import.meta.env.VITE_PLEX_PORT as string;
